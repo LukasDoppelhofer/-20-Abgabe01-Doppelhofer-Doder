fc25d81ba01ecfbf8171e9efbd9117963965d72f
doderben <benjamin.doder@edu.campus02.at>
2020-05-12 16:10:50 +0200

    added all getters and setters to Cocktail.java

--- src/main/java/at/fhj/iit/Cocktail.java
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -2,12 +2,25 @@ package at.fhj.iit;
 
 import java.util.ArrayList;
 
+/**
+ * Class represents a cocktail that contains several liquids
+ */
+
 public class Cocktail extends Drink{
+
+    /**
+     * enum that represents the color of the cocktail
+     */
+    private enum Color {
+        red, blue, green, yellow, clear
+    }
+    private Color cocktailColor;
 
     /**
      * List of liquids
      */
     private ArrayList<Liquid> liquidList;
+
     /**
      * volume of the cocktail
      */
@@ -21,10 +34,55 @@ public class Cocktail extends Drink{
      * Creates a Cocktail object with given name
      *
      * @param name name of the drink
+     * @param liquidList list of all liquids that are in the cocktail
      */
-    public Cocktail(String name) {
+    public Cocktail(String name, ArrayList<Liquid> liquidList, Color cocktailColor) {
         super(name);
+        this.liquidList = liquidList;
+        this.cocktailColor = cocktailColor;
     }
+
+    /**
+     * getter for cocktailColor
+     *
+     * @return cocktailColor of the cocktail
+     */
+    public Color getCocktailColor() {
+        return cocktailColor;
+    }
+
+    /**
+     * setter for cocktailColor
+     *
+     * @param cocktailColor new cocktailColor
+     */
+    public void setCocktailColor(Color cocktailColor) {
+        this.cocktailColor = cocktailColor;
+    }
+    /**
+     * getter for liquidList
+     *
+     * @return liquidList of the cocktail
+     */
+
+    public ArrayList<Liquid> getLiquidList() {
+        return liquidList;
+    }
+    /**
+     * setter for liquidList
+     *
+     * @param liquidList new liquidList
+     */
+    public void setLiquidList(ArrayList<Liquid> liquidList) {
+        this.liquidList = liquidList;
+    }
+    /**
+     * the following getters do not require an equivalent setter
+     * because these variables are calculated
+     * @param volume
+     * @param alcoholPercent
+     * @param isAlcoholic
+     */
 
     /**
      * Getter of volume
@@ -32,7 +90,7 @@ public class Cocktail extends Drink{
      */
     @Override
     public double getVolume() {
-        return 0;
+        return volume;
     }
     /**
      * Getter of AlcoholPercent
@@ -40,7 +98,7 @@ public class Cocktail extends Drink{
      */
     @Override
     public double getAlcoholPercent() {
-        return 0;
+        return alcoholPercent;
     }
     /**
      * Getter of isAlcoholic
@@ -48,6 +106,10 @@ public class Cocktail extends Drink{
      */
     @Override
     public boolean isAlcoholic() {
-        return false;
+        if(alcoholPercent > 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
