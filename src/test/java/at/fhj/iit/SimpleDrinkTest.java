74c3c4f1ae9081504f44b38539f6fe7c72a793ae
LukasDoppelhofer <lukas.doppelhofer@edu.campus02.at>
2020-05-12 16:51:02 +0200

    added java doc comment to SimpleDrinkTest.java

--- src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -6,15 +6,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Testing SimpleDrink implementation")
+
 public class SimpleDrinkTest {
-    private SimpleDrink lN, lA;
+    private SimpleDrink sW, sB;
+    /**
+     * inits an alcoholic and nonalcoholic SimpleDrink for EACH test
+     */
     @Test
     @BeforeEach
     void setup() {
         // SETUP PHASE
-        lN = new SimpleDrink("Water",new Liquid("Water",2.0,0));
-        lA = new SimpleDrink("Bier",new Liquid("Bier",2.0,0));
+        sW = new SimpleDrink("Water",new Liquid("Water",1.0,0));
+        sB = new SimpleDrink("Gösser",new Liquid("Beer",0.5,5));
     }
-
+    @Test
+    @DisplayName("Testing constructor alcohol")
+    public void testConstructorAlcohol(){
+        assertEquals(sB.getVolume(),0.5,0.001);
+        assertEquals(sB.getAlcoholPercent(),5);
+        assertEquals(sB.getName(),"Gösser");
+    }
+    @Test
+    @DisplayName("Testing constructor non alcohol")
+    public void testConstructorNonAlcohol(){
+        assertEquals(sW.getVolume(),1.0,0.001);
+        assertEquals(sW.getAlcoholPercent(),0);
+        assertEquals(sW.getName(),"Water");
+    }
 
 }
