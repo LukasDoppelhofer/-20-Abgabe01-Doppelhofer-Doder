74e10a0694002a03cfb25e38a3663864f170542d
LukasDoppelhofer <lukas.doppelhofer@edu.campus02.at>
2020-05-13 14:26:59 +0200

    added java doc comments to SimpleDrinkTest

--- src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * This class tests the class SimpleDrink
+ */
+
 @DisplayName("Testing SimpleDrink implementation")
 
 public class SimpleDrinkTest {
@@ -20,6 +24,9 @@ public class SimpleDrinkTest {
         sW = new SimpleDrink("Water",new Liquid("Water",1.0,0));
         sB = new SimpleDrink("Gösser",new Liquid("Beer",0.5,5));
     }
+    /**
+     * Tests the constructor of an alcoholic SimpleDrink
+     */
     @Test
     @DisplayName("Testing constructor alcohol")
     public void testConstructorAlcohol(){
@@ -27,6 +34,9 @@ public class SimpleDrinkTest {
         assertEquals(sB.getAlcoholPercent(),5);
         assertEquals(sB.getName(),"Gösser");
     }
+    /**
+     * Tests the constructor of an non alcoholic SimpleDrink
+     */
     @Test
     @DisplayName("Testing constructor non alcohol")
     public void testConstructorNonAlcohol(){
@@ -34,7 +44,9 @@ public class SimpleDrinkTest {
         assertEquals(sW.getAlcoholPercent(),0);
         assertEquals(sW.getName(),"Water");
     }
-
+    /**
+     * Tests the isAlcoholic method of a SimpleDrink
+     */
     @Test
     @DisplayName("Testing isAlcoholic method")
     public void testIsAlcoholic(){
